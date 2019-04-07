20a53bc99eb8e697470207fde2b1ba0226938e68
Polina <sloboda.polina@gmail.com>
2019-04-07 19:08:24 +0300

    final

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var STEPS = 3;
-var EPS = 0.0001;
+var EPS = 0.000001;
 var NODES = 8; // IT HAS TO BE SIZE OF p AND a
 
 // вероятности переходов между верщинами (состояниями)
@@ -29,72 +29,6 @@ function multiply(vec, mat) { //[line][column]
 	return result;
 }
 
-function countSquareDiff (prev, cur) {
-	var sum = 0;
-	for(var i = 0; i < NODES; i++) {
-		sum += (prev[i] - cur[i]) * (prev[i] - cur[i]);
-	}
-	return sum;
-}
-
-function analyseState(a) {
-	console.log("----- STARTING -----")
-	var next;
-	var diff = 2 * EPS;
-	while(diff > EPS) {
-		next = multiply(a, p);
-		diff = countSquareDiff(a, next);
-		console.log(diff);
-		a = next;
-	}
-	console.log("SOLUTION IS " + a);
-	console.log("----- FINISHED -----");
-	return a;
-}
-
-// анализируем первый вектор начальных состояний
-var test1 = analyseState([0, 0.1, 0.2, 0, 0.3, 0.1, 0, 0.3]);
-
-// а теперь второй
-var test2 = analyseState([0.1, 0.1, 0.1, 0, 0.2, 0.5, 0, 0]);
-
-
-
-// теперь аналитически.
-// уравнение pi = pi * p (Это значит, что вектор состояния не будет меняться)
-// pi * (E - p) = 0. Назовем (E - p) = B;    pi * B = 0
-function createSquareMatrix(side) {
-	var matrix = new Array(side);
-	for (var i = 0; i < side; i++) {
-		matrix[i] = new Array(side);
-	}
-	return matrix;
-}
-
-var B = createSquareMatrix(NODES);
-for (var i = 0; i < NODES; i++) {
-	for (var j = 0; j < NODES; j++) {
-		B[i][j] = - p[i][j];
-		if (i == j) {
-			B[i][j] += 1;
-		}
-	}
-}
-
-// pi * B = 0, получаем систему. А еще сумма координат pi должна быть 1.
-// поэтому можно заменить последнее уравнение,
-// поставить в B полследний столбец из 1, получить
-// pi * B = (0, 0, ..., 0, 1).
-// Это потеря информации, и меня смущает, что мы вообще производим эту операцию.
-// по идее, если так не сделать, то будет возможно одно из решений pi = 0,
-// и это нам явно не подходит
-for (var i = 0; i < NODES; i++) {
-	B[i][NODES - 1] = 1;
-}
-
-// надо заметить, что это будет эквивалентно Bt * pit = (0, 0, ..., 0, 1)t,
-// где pit- транспонированный вектор (вектор-столбец)
-// метод Крамера
 function Determinant(A)	{
 	var n = A.length, subA = [], detA = 0;
 	if (n==1) return A[0][0];
@@ -125,20 +59,88 @@ function Determinant(A)	{
 	return detA;
 }
 
+function countSquareDiff (prev, cur) {
+	var sum = 0;
+	for(var i = 0; i < NODES; i++) {
+		sum += (prev[i] - cur[i]) * (prev[i] - cur[i]);
+	}
+	return sum;
+}
+
+function analyseState(a) {
+	console.log("----- STARTING -----")
+	var next;
+	var diff = 2 * EPS;
+	while(diff > EPS) {
+		next = multiply(a, p);
+		diff = countSquareDiff(a, next);
+		console.log(diff);
+		a = next;
+	}
+	console.log("SOLUTION IS " + a);
+	console.log("----- FINISHED -----");
+	return a;
+}
+
+function createSquareMatrix(side) {
+	var matrix = new Array(side);
+	for (var i = 0; i < side; i++) {
+		matrix[i] = new Array(side);
+	}
+	return matrix;
+}
+
+// анализируем первый вектор начальных состояний
+var test1 = analyseState([0, 0.1, 0.2, 0, 0.3, 0.1, 0, 0.3]);
+
+// а теперь второй
+var test2 = analyseState([0.1, 0.1, 0.1, 0, 0.2, 0.5, 0, 0]);
+
+// теперь аналитически
+// уравнение pi = pi * p (Это значит, что вектор состояния не будет меняться)
+// оно эквивалентно pit = pt * pit, pit будут векторы - столбцы,
+// pt - транспонированная матрица p
+// (pt - E) * pit = 0. Назовем (pt - E) = B;    B * pit = 0
+var B = createSquareMatrix(NODES);
+for (var i = 0; i < NODES; i++) {
+	for (var j = 0; j < NODES; j++) {
+		B[i][j] = p[j][i];
+		if (i == j) {
+			B[i][j] -= 1;
+		}
+	}
+}
+
+// получаем систему. А еще сумма координат pit должна быть 1.
+// поэтому можно заменить последнее уравнение,
+// поставить в B последнюю строку из 1, получить
+// B * pit = (0, 0, ..., 0, 1)t.
+// Это потеря информации, и меня смущает, что мы вообще производим эту операцию.
+// по идее, если так не сделать, то будет возможно одно из решений pit = 0,
+// и это нам явно не подходит
+for (var i = 0; i < NODES; i++) {
+	B[NODES - 1][i] = 1;
+}
+
+// метод Крамера
 // Считаем определитель
-if (Determinant(B) != 0) {
+var detB = Determinant(B);
+if (detB != 0) {
 	console.log("----- ANALYTICS -----");
 	var result = [];
-	// меняем последовательно СТРОКИ B на свободные члены (тк В транспонирована)
+	// меняем последовательно столбцы B на свободные члены
 	// и считаем остальные определители
-	for(var i = 0; i < NODES; i++) {
-		result.push(
-			Determinant(
-				B.slice(0, i)
-				.concat([[0, 0, 0, 0, 0, 0, 0, 1]])
-				.concat(B.slice(i + 1, NODES))
-			)
-		);
+	var tmp = createSquareMatrix(NODES);
+	for (var k = 0; k < NODES; k++) { // меняем столбец k
+		for(var i = 0; i < NODES; i++) {
+			for(var j = 0; j < NODES; j++) {
+				tmp[i][j] = B[i][j];
+				if (j == k) {
+					tmp[i][j] = i < NODES - 1 ? 0 : 1;
+				}
+			}
+		}
+		result.push(Determinant(tmp)/detB);
 	}
 	console.log("SOLUTION IS " + result);
 	console.log("SQUAREDIFF FROM TEST 1: " + countSquareDiff(result, test1));
